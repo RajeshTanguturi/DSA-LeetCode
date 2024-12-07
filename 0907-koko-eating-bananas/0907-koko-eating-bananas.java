b22225a64db64295de44cf3d7014071f1c148650
Rajesh Tanguturi <135504267+RajeshTanguturi@users.noreply.github.com>
2024-12-07 23:52:42 +0530

    Time: 0 ms (100%), Space: 45.3 MB (19.83%) - LeetHub

--- 0907-koko-eating-bananas/0907-koko-eating-bananas.java
+++ 0907-koko-eating-bananas/0907-koko-eating-bananas.java
@@ -1,29 +1,56 @@
 class Solution {
-    private int timetaken(int arr[], int rate){
-        int total = 0;
-        for(int i = 0 ; i< arr.length ; i++){
-            total+= Math.ceil((double)arr[i]/rate);
-        }
-        return total;
-    }
     public int minEatingSpeed(int[] piles, int h) {
-        int maxele = 0; 
-        for(int i : piles){
-            maxele = Math.max(maxele,i);
-        }
-        int left = 1,right = maxele;
-        while(left <= right){
-            int mid = left+(right - left)/2;
-            int time = timetaken(piles,mid);
-            System.out.println(mid + " " +time);
+      int n = piles.length;
+        long total = 0;
+        for (int p : piles) total += p;
 
-            if(time <= h){
-                right = mid - 1 ; 
+        int left = (int) ((total - 1) / h) + 1;
+        int right = (int) ((total - n) / (h - n + 1)) + 1;
+
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            int time = 0;
+            for (int p : piles) {
+                time += (p - 1) / mid + 1;
             }
-            else if(time > h){
-               left  = mid+1;
+            if (time > h) {
+                left = mid + 1;
+            } else {
+                right = mid;
             }
         }
-        return left;
+        
+        return left;  
     }
 }
+
+
+// class Solution {
+//     private int timetaken(int arr[], int rate){
+//         int total = 0;
+//         for(int i = 0 ; i< arr.length ; i++){
+//             total+= Math.ceil((double)arr[i]/rate);
+//         }
+//         return total;
+//     }
+//     public int minEatingSpeed(int[] piles, int h) {
+//         int maxele = 0; 
+//         for(int i : piles){
+//             maxele = Math.max(maxele,i);
+//         }
+//         int left = 1,right = maxele;
+//         while(left <= right){
+//             int mid = left+(right - left)/2;
+//             int time = timetaken(piles,mid);
+//             System.out.println(mid + " " +time);
+
+//             if(time <= h){
+//                 right = mid - 1 ; 
+//             }
+//             else if(time > h){
+//                left  = mid+1;
+//             }
+//         }
+//         return left;
+//     }
+// }
