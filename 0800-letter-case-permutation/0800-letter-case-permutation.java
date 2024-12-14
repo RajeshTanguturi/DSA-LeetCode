fcb89c4e7d33781f9132d399419938510563b1b2
Rajesh Tanguturi <135504267+RajeshTanguturi@users.noreply.github.com>
2024-12-14 15:53:29 +0530

    Time: 1 ms (100%), Space: 45.8 MB (14.37%) - LeetHub

--- 0800-letter-case-permutation/0800-letter-case-permutation.java
+++ 0800-letter-case-permutation/0800-letter-case-permutation.java
@@ -1,29 +1,22 @@
 class Solution {
     public List<String> letterCasePermutation(String s) {
         List<String> ans = new ArrayList<>();
-        ans.add(s);
-        backtrack(s,ans,0);
+        backtrack(s.toCharArray(),ans,0);
         return ans;
     }
-    public void backtrack(String s, List<String> ans,int idx){
-        if(idx == s.length()){
+    public void backtrack(char[] s, List<String> ans,int idx){
+        if(idx == s.length){
+            ans.add(new String(s));
             return;
         }
-        for(int i = idx ; i<s.length() ; i++){
-            char c = s.charAt(i);
-            if(c>='a' && c<='z'){
-                c-=32;
-                String modifiedString = s.substring(0,i)+c+s.substring(i+1);
-                ans.add(modifiedString);
-                backtrack(modifiedString,ans,i+1);
-                continue;
-            }else if(c>='A' && c<='Z'){
-                c+=32;
-                String modifiedString = s.substring(0,i)+c+s.substring(i+1);
-                ans.add(modifiedString);
-                backtrack(modifiedString,ans,i+1);
-                continue;
-            }
+        if(Character.isLetter(s[idx])){
+            s[idx] = Character.toLowerCase(s[idx]);
+            backtrack(s,ans,idx+1);
+            s[idx] = Character.toUpperCase(s[idx]);
+            backtrack(s,ans,idx+1);
+        }
+        else{
+            backtrack(s,ans,idx+1);
         }
     }
 }
