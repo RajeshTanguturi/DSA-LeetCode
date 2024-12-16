e13f69b4d16a23a985fe9605a4f8b01d55232c05
Rajesh Tanguturi <135504267+RajeshTanguturi@users.noreply.github.com>
2024-12-16 15:57:55 +0530

    Time: 1 ms (99.82%), Space: 44.9 MB (38.52%) - LeetHub

--- 0047-permutations-ii/0047-permutations-ii.java
+++ 0047-permutations-ii/0047-permutations-ii.java
@@ -1,16 +1,19 @@
 class Solution {
     public List<List<Integer>> permuteUnique(int[] nums) {
-        Set<List<Integer>> ans = new HashSet<>();
+        List<List<Integer>> ans = new ArrayList<>();
+        Arrays.sort(nums);
         dfs(nums,ans,new ArrayList<>(),new boolean[nums.length]);
-        return new ArrayList<>(ans);
+        return ans;
     }
-    public void dfs(int[] nums, Set<List<Integer>> ans, List<Integer> curr, boolean[] visited){
+    public void dfs(int[] nums, List<List<Integer>> ans, List<Integer> curr, boolean[] visited){
         if(curr.size() == nums.length){
             ans.add(new ArrayList<>(curr));
             return;
         }
         for(int i =0 ; i< nums.length ; i++){
-            if(!visited[i]){
+            if(visited[i]) continue;
+            else if (i > 0 && nums[i] == nums[i - 1] && !visited[i - 1]) continue;
+            else{
                 visited[i] = true;
                 curr.add(nums[i]);
                 dfs(nums,ans,curr,visited);
