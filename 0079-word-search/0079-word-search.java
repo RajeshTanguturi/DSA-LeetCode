06fb1e2cf1a144eb37e7969d35014f609d64e1fb
Rajesh Tanguturi <135504267+RajeshTanguturi@users.noreply.github.com>
2024-12-17 13:17:58 +0530

    Time: 141 ms (66.34%), Space: 41.5 MB (71.84%) - LeetHub

--- 0079-word-search/0079-word-search.java
+++ 0079-word-search/0079-word-search.java
@@ -1,12 +1,12 @@
 class Solution {
-    public boolean searchword(char[][] board, String word, boolean[][] visited, int row, int col, int idx) {
+    public boolean searchword(char[][] board, String word, int row, int col, int idx) {
         // Base case: If we matched all characters in the word
         if (idx == word.length()) {
             return true;
         }
 
         // Out-of-bounds or already visited
-        if (row < 0 || row >= board.length || col < 0 || col >= board[0].length || visited[row][col]) {
+        if (row < 0 || row >= board.length || col < 0 || col >= board[0].length ) {
             return false;
         }
 
@@ -16,16 +16,16 @@ class Solution {
         }
 
         // Mark the cell as visited
-        visited[row][col] = true;
+        board[row][col] = '#';
 
         // Explore all 4 directions
-        boolean found = searchword(board, word, visited, row - 1, col, idx + 1) || // Up
-                        searchword(board, word, visited, row + 1, col, idx + 1) || // Down
-                        searchword(board, word, visited, row, col - 1, idx + 1) || // Left
-                        searchword(board, word, visited, row, col + 1, idx + 1);   // Right
+        boolean found = searchword(board, word, row - 1, col, idx + 1) || // Up
+                        searchword(board, word, row + 1, col, idx + 1) || // Down
+                        searchword(board, word, row, col - 1, idx + 1) || // Left
+                        searchword(board, word, row, col + 1, idx + 1);   // Right
 
         // Backtrack: Unmark the cell as visited
-        visited[row][col] = false;
+        board[row][col] = word.charAt(idx);
 
         return found;
     }
@@ -37,7 +37,7 @@ class Solution {
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
                 if (board[i][j] == word.charAt(0)) { // Start search from matching first character
-                    if (searchword(board, word, new boolean[m][n], i, j, 0)) { // Start idx = 0
+                    if (searchword(board, word, i, j, 0)) { // Start idx = 0
                         return true;
                     }
                 }
