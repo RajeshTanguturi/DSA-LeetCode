762ba58cb93d12f286f5ad31efcc8a6deb580226
Rajesh Tanguturi <135504267+RajeshTanguturi@users.noreply.github.com>
2024-12-17 09:39:43 +0530

    Time: 1 ms (93.97%), Space: 44.6 MB (29.31%) - LeetHub

--- 0046-permutations/0046-permutations.java
+++ 0046-permutations/0046-permutations.java
@@ -1,22 +1,29 @@
 class Solution {
     public List<List<Integer>> permute(int[] nums) {
-        List<List<Integer>> list = new ArrayList<>();
-        // Arrays.sort(nums); // not necessary
-        backtrack(list, new ArrayList<>(), nums);
-        return list;
+        List<List<Integer>> ans = new ArrayList<>();
+        dfsWithSwap(nums,new ArrayList<>(),ans,0);
+        return ans;
     }
-
-    private void backtrack(List<List<Integer>> list, List<Integer> tempList, int[] nums) {
-        if (tempList.size() == nums.length) {
-            list.add(new ArrayList<>(tempList));
-        } else {
-            for (int i = 0; i < nums.length; i++) {
-                if (tempList.contains(nums[i]))
-                    continue; // element already exists, skip
-                tempList.add(nums[i]);
-                backtrack(list, tempList, nums);
-                tempList.remove(tempList.size() - 1);
+    public void dfsWithSwap(int nums[], List<Integer> curr, List<List<Integer>> ans, int idx){
+        if(idx == nums.length){
+            List<Integer> sol  = new ArrayList<>();
+            for(int i = 0;i< nums.length ; i++){
+                sol.add(nums[i]);
             }
+            ans.add(new ArrayList<>(sol));
+            return;
+        }
+
+        for(int i = idx ; i < nums.length ; i++){
+            //swap idx and i 
+            int temp = nums[i];
+            nums[i] = nums[idx];
+            nums[idx] = temp;
+            dfsWithSwap(nums,curr,ans,idx+1);
+            //unswap 
+            temp = nums[i];
+            nums[i] = nums[idx];
+            nums[idx] = temp;
         }
     }
 }
