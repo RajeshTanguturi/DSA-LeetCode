35c5239a60c2c49846820a27f0a2a28b2dd6689f
Rajesh Tanguturi <135504267+RajeshTanguturi@users.noreply.github.com>
2024-12-26 19:59:54 +0530

    Time: 1 ms (99.03%), Space: 42.3 MB (10.59%) - LeetHub

--- 1626-can-make-arithmetic-progression-from-sequence/1626-can-make-arithmetic-progression-from-sequence.java
+++ 1626-can-make-arithmetic-progression-from-sequence/1626-can-make-arithmetic-progression-from-sequence.java
@@ -1,11 +1,53 @@
 class Solution {
     public boolean canMakeArithmeticProgression(int[] arr) {
-        Arrays.sort(arr);
+        // Arrays.sort(arr);
+        mergeSort(arr,0,arr.length-1);
         int diff = arr[0] - arr[1];
         for(int i = 2 ;i<arr.length ; i++){
             if(arr[i-1] - arr[i] != diff) return false;
         }
         return true;
+    }
+    public void mergeSort(int[] arr, int left, int right){
+        if(left == right) return;
+        int mid = left + (right - left)/2;
+        mergeSort(arr,left,mid);
+        mergeSort(arr,mid+1,right);
+        merge(arr,left, right , mid);
+    }
+    public void merge(int arr[], int l , int r, int m){
+        int n1 = m-l+1;
+        int n2 = r -m;
+        int leftarr[] = new int[n1];
+        int rightarr[] = new int[n2];
+
+        for(int i = 0 ; i< n1 ; i++){
+            leftarr[i] = arr[l+i];
+        }
+        for(int i = 0 ; i < n2 ; i++){
+            rightarr[i] = arr[m+1+i];
+        }
+        int i = 0 , j = 0 , k = l;
+        while(i<n1 && j < n2){
+            if(leftarr[i]<= rightarr[j]){
+                arr[k] = leftarr[i];
+                i++;
+            }else{
+                arr[k] = rightarr[j];
+                j++;
+            }
+            k++;    
+        }
+        while(i<n1){
+            arr[k] = leftarr[i];
+            i++;
+            k++;
+        }
+        while(j<n2){
+            arr[k] = rightarr[j];
+            j++;
+            k++;
+        }
     }
 
 }
