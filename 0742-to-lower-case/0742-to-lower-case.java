e8d9930d006dd5defc3d24dc8384356ed50f426e
Rajesh Tanguturi <135504267+RajeshTanguturi@users.noreply.github.com>
2024-12-26 21:22:59 +0530

    Time: 0 ms (100%), Space: 41.8 MB (24.3%) - LeetHub

--- 0742-to-lower-case/0742-to-lower-case.java
+++ 0742-to-lower-case/0742-to-lower-case.java
@@ -1,5 +1,10 @@
 class Solution {
     public String toLowerCase(String s) {
-        return s.toLowerCase();
+        StringBuilder sb = new StringBuilder();
+        for(char c : s.toCharArray()){
+            if(c>=65 && c<=90) c+=32;
+            sb.append(c);
+        }
+        return sb.toString();
     }
 }
