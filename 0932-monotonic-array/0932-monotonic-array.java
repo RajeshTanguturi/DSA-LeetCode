50a29934f58926946100a607bc89f9dbdc59da7c
Rajesh Tanguturi <135504267+RajeshTanguturi@users.noreply.github.com>
2024-12-28 19:21:43 +0530

    Time: 3 ms (45.32%), Space: 59.9 MB (7.24%) - LeetHub

--- 0932-monotonic-array/0932-monotonic-array.java
+++ 0932-monotonic-array/0932-monotonic-array.java
@@ -1,32 +1,16 @@
 class Solution {
     public boolean isMonotonic(int[] nums) {
-        int i = 0;
-
-        //traversing till nums[i]!=nums[i+1]
-        while(i < nums.length-1){
-            if(nums[i]!=nums[i+1]) break;
-            i++;
+       boolean increases = true;
+       boolean decreases = true;
+       for(int i = 1 ; i< nums.length ; i++){
+        if(nums[i] > nums[i-1]){
+            decreases = false;
         }
-        if(i==nums.length -1) return true;
-        if(nums[i] > nums[i+1]){
-            //checkdecresing
-            // int max = nums[i];
-            int prev = nums[i];
-            for(int j = i; j< nums.length ; j++){
-                if( nums[j]>prev) return false;
-                prev = nums[j];
-            }
-            return true;
-        }else{
-            System.out.println("second case");
-            // int min = nums[i];
-            int prev = nums[i];
-             for(int j = i; j< nums.length ; j++){
-            // System.out.println(i+" "+ prev);
-                if(nums[j]< prev) return false;
-                prev = nums[j];
-            }
-            return true;
+        if(nums[i] < nums[i-1]){
+            increases = false;
         }
+       }
+       return increases || decreases;
+        
     }
 }
