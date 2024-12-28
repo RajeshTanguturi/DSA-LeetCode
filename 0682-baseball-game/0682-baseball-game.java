5e5a5c5ac5fb45576b78d9dd1e6f031be31c1e15
Rajesh Tanguturi <135504267+RajeshTanguturi@users.noreply.github.com>
2024-12-28 19:35:41 +0530

    Time: 2 ms (93.43%), Space: 41.9 MB (35.05%) - LeetHub

--- 0682-baseball-game/0682-baseball-game.java
+++ 0682-baseball-game/0682-baseball-game.java
@@ -4,24 +4,24 @@ class Solution {
         for(String s: operations){
             if(s.equals("C")){
                 res.remove(res.size() - 1);
-                System.out.println(res);
+                // System.out.println(res);
             }
             else if(s.equals("D")){
                 res.add(2*res.get(res.size()-1));
-                System.out.println(res);
+                // System.out.println(res);
             }
             else if(s.equals("+")){
                 int lastIdx = res.size() - 1;
                 res.add(res.get(lastIdx)+res.get(lastIdx-1));
-                System.out.println(res);
+                // System.out.println(res);
             }else{
                 int score = Integer.parseInt(s);
                 // System.out.println(score);
                 res.add(score);
-                System.out.println(res);
+                // System.out.println(res);
             }
         }
-        System.out.println(res);
+        // System.out.println(res);
 
         return sum(res);
     }
