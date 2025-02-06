63a52e64ee9be0c4781db8a5756492229d30cc88
Rajesh Tanguturi <135504267+RajeshTanguturi@users.noreply.github.com>
2025-02-06 16:27:14 +0530

    Time: 10 ms (84.98%), Space: 48.9 MB (42.27%) - LeetHub

---
+++ 0744-network-delay-time/0744-network-delay-time.java
@@ -0,0 +1,61 @@
+class Solution {
+    public int minDist(int[] dist,boolean sptSet[]){
+        int min = Integer.MAX_VALUE;
+        int minIdx = 0;
+        for(int i = 0 ; i < dist.length ; i++){
+            if(!sptSet[i] && dist[i] < min){
+                min = dist[i];
+                minIdx = i;
+            }
+        }
+        return minIdx;
+    }
+    public boolean allVisited(boolean sptSet[]){
+        for( boolean b : sptSet){
+            if(!b) return false;
+        }
+        return true;
+    }
+    public int networkDelayTime(int[][] times, int n, int k) {
+
+        Map<Integer, List<int[]>> adj = new HashMap<>();
+        for (int i = 1; i <= n; i++) {
+            adj.put(i, new ArrayList<>());
+        }
+        for (int[] time : times) {
+            adj.get(time[0]).add(new int[]{time[1], time[2]});
+        }
+        // int n = adj.size();
+        int[] dist = new int[n + 1];
+        Arrays.fill(dist, Integer.MAX_VALUE);
+        dist[k] = 0;  // Source node distance = 0
+        
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> a[0] - b[0]);
+        pq.offer(new int[]{0, k});
+
+        while (!pq.isEmpty()) {
+            int[] curr = pq.poll();
+            int time = curr[0];
+            int node = curr[1];
+
+            if (time > dist[node]) continue; // Skip if we already have a better distance
+
+            for (int[] neighbor : adj.get(node)) {
+                int nextNode = neighbor[0];
+                int weight = neighbor[1];
+
+                if (dist[node] + weight < dist[nextNode]) {
+                    dist[nextNode] = dist[node] + weight;
+                    pq.offer(new int[]{dist[nextNode], nextNode});
+                }
+            }
+        }
+        int maxTime = 0;
+        for (int i = 1; i <= n; i++) {
+            if (dist[i] == Integer.MAX_VALUE) return -1; // If any node is unreachable
+            maxTime = Math.max(maxTime, dist[i]);
+        }
+
+        return maxTime;
+    }
+}
