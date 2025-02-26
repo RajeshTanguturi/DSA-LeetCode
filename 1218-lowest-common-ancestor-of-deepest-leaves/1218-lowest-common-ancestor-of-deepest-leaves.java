9c68cd94ae8798b01f66dcf45c73f210d94ac82a
Rajesh Tanguturi <135504267+RajeshTanguturi@users.noreply.github.com>
2025-02-26 22:32:53 +0530

    Time: 0 ms (100%), Space: 44.8 MB (11.98%) - LeetHub

--- 1218-lowest-common-ancestor-of-deepest-leaves/1218-lowest-common-ancestor-of-deepest-leaves.java
+++ 1218-lowest-common-ancestor-of-deepest-leaves/1218-lowest-common-ancestor-of-deepest-leaves.java
@@ -22,20 +22,20 @@ class Solution {
     }
     public int  dfs(TreeNode root, int depth){
         if(root == null) return 0;
-        if( root.left == null && root.right == null){
+        if( root.left == null && root.right == null){ //
             if(depth >= maxDepth){
-            maxDepth = depth;
-            ans = root;
+                maxDepth = depth;
+                ans = root;
             } 
             return depth;
         }
         int left = dfs(root.left, depth+1);
         int right = dfs(root.right, depth+1);
         
-        if(left == right && left == maxDepth){
+        if(right == maxDepth && left == maxDepth){ // check if left == right == maxDepth
             ans = root;
         }
-        return Math.max(left,right);
+        return Math.max(left,right); // becoz to get maximum depth encountered from the current node
 
     }
 }
