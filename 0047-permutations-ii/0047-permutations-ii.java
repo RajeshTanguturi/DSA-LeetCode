84c0a6d41344ee69ba0f43054eea1fb86181f3e2
Rajesh Tanguturi <135504267+RajeshTanguturi@users.noreply.github.com>
2025-08-10 09:15:48 +0530

    Time: 2 ms (71.25%), Space: 44.8 MB (72.38%) - LeetHub

--- 0047-permutations-ii/0047-permutations-ii.java
+++ 0047-permutations-ii/0047-permutations-ii.java
@@ -1,25 +1,25 @@
 class Solution {
     public List<List<Integer>> permuteUnique(int[] nums) {
-        List<List<Integer>> ans = new ArrayList<>();
         Arrays.sort(nums);
-        dfs(nums,ans,new ArrayList<>(),new boolean[nums.length]);
+        List<List<Integer>> ans = new ArrayList<>();
+
+        solve(nums, new ArrayList<>(), ans , new boolean[nums.length]);
         return ans;
     }
-    public void dfs(int[] nums, List<List<Integer>> ans, List<Integer> curr, boolean[] visited){
-        if(curr.size() == nums.length){
+    private void solve( int[] nums, List<Integer> curr, List<List<Integer>> ans, boolean[] visited){
+        if( curr.size() == nums.length){
             ans.add(new ArrayList<>(curr));
             return;
         }
-        for(int i =0 ; i< nums.length ; i++){
-            if(visited[i]) continue;
-            else if (i > 0 && nums[i] == nums[i - 1] && !visited[i - 1]) continue;
-            else{
-                visited[i] = true;
-                curr.add(nums[i]);
-                dfs(nums,ans,curr,visited);
-                curr.remove(curr.size() - 1);
-                visited[i] = false;
-            }
+
+        for( int i = 0 ; i < nums.length ; i++){
+            if(visited[i] == true) continue;
+            if(i > 0 && nums[i-1] == nums[i] && visited[i-1] == false ) continue;
+            curr.add(nums[i]);
+            visited[i] = true;
+            solve( nums, curr, ans, visited);
+            visited[i] = false;
+            curr.remove(curr.size()-1);
         }
     }
 }
